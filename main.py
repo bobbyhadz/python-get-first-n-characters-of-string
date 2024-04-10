7ff49690467028af0b9728542c184c86e400ee33
bobbyhadz <opmftw@gmail.com>
2024-04-10 11:22:06 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# Get the first N characters of a String in Python
+
+string = 'bobbyhadz.com'
+
+first_char = string[0]
+print(first_char)  # 👉️ b
+
+# ✅ get the first 2 characters of a string
+first_2 = string[:2]
+print(first_2)  # 👉️ bo
+
+# ✅ get the first 3 characters of a string
+first_3 = string[:3]
+print(first_3)  # 👉️ bob
+
+# ✅ get the first 5 characters of a string
+first_5 = string[:5]
+print(first_5)  # 👉️ bobby
